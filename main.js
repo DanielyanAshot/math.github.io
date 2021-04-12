5a71500ddea4b7225c9e21da977898eb0bc936ec
DanielyanAshot <82168163+DanielyanAshot@users.noreply.github.com>
2021-04-13 01:08:40 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import {PI, pow, hypotenuse, circleArea} from "Math.js"
-import format from "format.js"
+import {PI, pow, hypotenuse, circleArea} from "./math.js";
+import {format} from "./format.js";
 
 console.log(format("an area of a circle with a radius of 8", circleArea(8)));
 console.log(format("hypotenuse of a triangle with sides 8 and 11", hypotenuse(8, 11).hypotenuse));
