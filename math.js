7f5eb9c001bd81e3af13b56bc69d3dc6ffa92e09
DanielyanAshot <82168163+DanielyanAshot@users.noreply.github.com>
2021-04-13 01:09:02 +0400

    Update math.js

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
-const PI = 3.14;
+export const PI = 3.14;
 
-function pow (base, exponent = 2){
+export function pow (base, exponent = 2){
  
   if (exponent === 0){
     return 1;
@@ -8,7 +8,7 @@ function pow (base, exponent = 2){
   return base * pow(base, exponent-1);
 }
 
-function hypotenuse (adjacent, opposite){
+export function hypotenuse (adjacent, opposite){
 
   return {
           adjacent,
@@ -17,8 +17,6 @@ function hypotenuse (adjacent, opposite){
          };
 }
 
-function circleArea (radius){
+export function circleArea (radius){
   return PI * radius**2;
 }
-
-export {PI, pow, hypotenuse, circleArea};
