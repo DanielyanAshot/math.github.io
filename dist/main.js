2e5ffd8cc1f991b868769345c86c6004f2fbc29a
DanielyanAshot <82168163+DanielyanAshot@users.noreply.github.com>
2021-04-13 01:09:49 +0400

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,9 @@
 "use strict";
 
-var _Math = require("Math.js");
+var _math = require("./math.js");
 
-var _format = _interopRequireDefault(require("format.js"));
+var _format = require("./format.js");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-console.log((0, _format["default"])("an area of a circle with a radius of 8", (0, _Math.circleArea)(8)));
-console.log((0, _format["default"])("hypotenuse of a triangle with sides 8 and 11", (0, _Math.hypotenuse)(8, 11).hypotenuse));
-console.log((0, _format["default"])("5 to the power of 9", (0, _Math.pow)(5, 9)));
+console.log((0, _format.format)("an area of a circle with a radius of 8", (0, _math.circleArea)(8)));
+console.log((0, _format.format)("hypotenuse of a triangle with sides 8 and 11", (0, _math.hypotenuse)(8, 11).hypotenuse));
+console.log((0, _format.format)("5 to the power of 9", (0, _math.pow)(5, 9)));
